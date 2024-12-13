663414d5df64fc2f8d3b4ee994224a1cf85dd1c4
Amélie <amelie.nader.prieto@alumnos.upm.es>
2024-12-13 13:53:08 +0100

    Entidad para devolver el dinero
    Esta entidad es una máquina de estados. Enciende una secuencia de LEDs a medida que va devolviendo las monedas. La he simulado y su funcionamiento es el esperado.

---
+++ Trabajo_SED/Trabajo_SED.srcs/sim_1/new/devolver_el_dinero_tb.vhd
@@ -0,0 +1,54 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity devolver_el_dinero_tb is
+--  Port ( );
+end devolver_el_dinero_tb;
+
+architecture Behavioral of devolver_el_dinero_tb is
+
+component devolver_el_dinero is
+    Port ( cantidad : in integer;
+           enable : in STD_LOGIC;
+           clk : in STD_LOGIC;
+           cantidad_restante : out integer;
+           devolver_monedas : out std_logic_vector(3 downto 0));
+end component;
+
+signal cantidad : integer := 0;
+signal clk : std_logic := '0';
+signal enable : std_logic := '0';
+signal monedas : std_logic_vector(3 downto 0);
+signal cantidad_restante : integer;
+
+begin
+
+uut : devolver_el_dinero port map (cantidad => cantidad, enable => enable, clk => clk, cantidad_restante => cantidad_restante, devolver_monedas => monedas);
+
+clk <= not clk after 10ns;
+
+stimuli : process
+begin
+
+    wait for 25ns; enable <= '1';
+    
+    -- probamos a introducir una cantidad a ver si la devuelve correctamente
+    cantidad <= 120; -- debería devolver 100 y luego 20
+    wait for 100ns;
+    cantidad <= 280; -- debería devolver 100, 100, 50, 20 y 10
+    wait for 200ns;
+    
+    -- probamos a introducir una cantidad y cambiarla antes de que termine de devolverla
+    cantidad <= 2000;
+    wait for 75ns; -- debería devolver 100 varias veces
+    cantidad <= 2500; -- debería actualizarse inmediatemente
+    wait for 500ns; -- debería devolver 100 varias veces
+    
+    -- si desactivamos el enable, deberían dejar de actualizarse los estados
+    enable <= '0'; -- las salidas deberían quedarse congeladas
+    wait for 100ns;
+    
+end process;
+
+end Behavioral;
---
+++ Trabajo_SED/Trabajo_SED.srcs/sources_1/new/devolver_el_dinero.vhd
@@ -0,0 +1,62 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity devolver_el_dinero is
+    Port ( cantidad : in integer;
+           enable : in STD_LOGIC;
+           clk : in STD_LOGIC;
+           cantidad_restante : out integer;
+           devolver_monedas : out std_logic_vector(3 downto 0)
+           );
+end devolver_el_dinero;
+
+architecture Behavioral of devolver_el_dinero is
+
+    type states is (reposo, devolver100, devolver50, devolver20, devolver10, entrada_cambiada);
+    signal current_state : states := reposo;
+    signal next_state : states;
+    signal dinero_restante : integer := 0;
+
+begin
+
+    state_reg : process(clk)
+    begin
+    if enable = '1' then
+        if rising_edge(clk) then current_state <= next_state;
+        end if;
+    end if;
+    end process;
+    
+    nextstate_decod : process(current_state, cantidad)
+    begin
+        if cantidad'event then next_state <= entrada_cambiada;
+        elsif current_state = reposo then
+            if dinero_restante >= 100 then next_state <= devolver100;
+            elsif dinero_restante >= 50 then next_state <= devolver50;
+            elsif dinero_restante >= 20 then next_state <= devolver20;
+            elsif dinero_restante >= 10 then next_state <= devolver10;
+            else next_state <= reposo;
+            end if;
+        else next_state <= reposo;
+        end if;
+    end process;
+    
+    output_decod : process(current_state)
+    begin
+    
+        case current_state is
+            when reposo => devolver_monedas <= "0000";
+            when entrada_cambiada => devolver_monedas <= "0000"; dinero_restante <= cantidad;
+            when devolver100 => devolver_monedas <= "1000"; dinero_restante <= dinero_restante - 100;
+            when devolver50 => devolver_monedas <= "0100"; dinero_restante <= dinero_restante - 50;
+            when devolver20 => devolver_monedas <= "0010"; dinero_restante <= dinero_restante - 20;
+            when devolver10 => devolver_monedas <= "0001"; dinero_restante <= dinero_restante - 10;
+            when others => devolver_monedas <= "0000";
+        end case;
+    
+    end process;
+    
+    cantidad_restante <= dinero_restante;
+
+end Behavioral;
