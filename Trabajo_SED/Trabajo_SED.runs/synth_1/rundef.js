fee5f09840077467081b61ed0e363b2d43ad2b82
SergioCantalejoVillasante <s.cantalejo@alumnos.upm.es>
2024-12-20 12:31:20 +0100

    cambio de interruptor
    Co-Authored-By: Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>

--- Trabajo_SED/Trabajo_SED.runs/synth_1/rundef.js
+++ Trabajo_SED/Trabajo_SED.runs/synth_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log Parte_logica.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source Parte_logica.tcl" );
+         "-log pulsadores.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source pulsadores.tcl" );
 
 
 
--- Trabajo_SED/Trabajo_SED.srcs/sources_1/new/interruptores.vhd
+++ Trabajo_SED/Trabajo_SED.srcs/sources_1/new/interruptores.vhd
@@ -4,13 +4,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity interruptores is
   Port (
         clk:in std_logic;
-        interruptor_asyn: in std_logic_vector;
-        interruptor_syn: out std_logic_vector
+        interruptor_asyn: in std_logic_vector (3 downto 0);
+        interruptor_syn: out std_logic_vector (3 downto 0)
 );
 end interruptores;
 
 architecture Behavioral of interruptores is
-signal syncout_edgein: std_logic_vector ;
+signal syncout_edgein: std_logic_vector (3 downto 0);
 
 
 COMPONENT SYNCHRNZR
@@ -30,15 +30,46 @@ EDGE: out std_logic
 END COMPONENT;
 
 begin
-    Inst_SYNCHRNZR: SYNCHRNZR PORT MAP(
+    Inst_SYNCHRNZR_3: SYNCHRNZR PORT MAP(
         clk=>clk,
-        ASYNC_IN=> interruptor_asyn,
-        SYNC_OUT=> syncout_edgein 
+        ASYNC_IN => interruptor_asyn(3),
+        SYNC_OUT => syncout_edgein(3)
     );
-    Inst_EDGEDTCTR: edgedtctr PORT MAP(
+    Inst_SYNCHRNZR_2: SYNCHRNZR PORT MAP(
+        clk=>clk,
+        ASYNC_IN => interruptor_asyn(2),
+        SYNC_OUT => syncout_edgein(2)
+    );
+    Inst_SYNCHRNZR_1: SYNCHRNZR PORT MAP(
+        clk=>clk,
+        ASYNC_IN=> interruptor_asyn (1),
+        SYNC_OUT=> syncout_edgein (1)
+    );
+    Inst_SYNCHRNZR_0: SYNCHRNZR PORT MAP(
+        clk=>clk,
+        ASYNC_IN=> interruptor_asyn (0),
+        SYNC_OUT=> syncout_edgein (0)
+    );
+    
+    Inst_EDGEDTCTR_3: edgedtctr PORT MAP(
         clk =>clk,
-        SYNC_IN=>syncout_edgein,
-        edge=> interruptor_syn
+        SYNC_IN=>syncout_edgein (3),
+        edge=> interruptor_syn (3)
+    );
+    Inst_EDGEDTCTR_2: edgedtctr PORT MAP(
+        clk =>clk,
+        SYNC_IN=>syncout_edgein (2),
+        edge=> interruptor_syn (2)
     ); 
+    Inst_EDGEDTCTR_1: edgedtctr PORT MAP(
+        clk =>clk,
+        SYNC_IN=>syncout_edgein (1),
+        edge=> interruptor_syn (1)
+    ); 
+    Inst_EDGEDTCTR_0: edgedtctr PORT MAP(
+        clk =>clk,
+        SYNC_IN=>syncout_edgein (0),
+        edge=> interruptor_syn (0)
+    );  
 
 end Behavioral;
